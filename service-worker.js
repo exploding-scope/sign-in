5723bd7c0fa965831c7879ee6fcd42f207590497
exploding-scope <kvalji@outlook.com>
2024-08-17 17:37:15 -0600

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -20,3 +20,35 @@ self.addEventListener('fetch', (event) => {
     })
   );
 });
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      if (response) {
+        return response;
+      }
+      return fetch(event.request).then((response) => {
+        const clonedResponse = response.clone();
+        const headers = new Headers(clonedResponse.headers);
+        headers.set('Cache-Control', 'max-age=3600');
+        return new Response(clonedResponse.body, { headers });
+      });
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      if (response) {
+        return response;
+      }
+      return fetch(event.request).then((response) => {
+        const clonedResponse = response.clone();
+        const headers = new Headers(clonedResponse.headers);
+        headers.set('X-Content-Type-Options', 'nosniff');
+        return new Response(clonedResponse.body, { headers });
+      });
+    })
+  );
+});
